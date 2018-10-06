3f106b6e965389d577df1af9dc0956819cf9472e
Golemh <34375335+Golemh@users.noreply.github.com>
2018-10-07 01:52:18 +0500

    Update index.php

--- Retailer/product/index.php
+++ Retailer/product/index.php
@@ -5,7 +5,7 @@
   session_start();
   include 'connection.php';
   if (!isset($_SESSION['username'])):
-    header("Location: http://localhost/Retailer/LoginPage/LoginPage.php");
+    header("Location: /dbSite/Retailer/LoginPage/LoginPage.php");
   
  else:
 ;?>
@@ -29,7 +29,7 @@
       <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="http://localhost/Retailer/LoginPage/AdminPanel.html">
+            <a class="nav-link" href="/dbSite/Retailer/LoginPage/AdminPanel.html">
               <i class="fa d-inline fa-lg fa-bookmark-o"></i> Admin panel</a>
           </li>
           <li class="nav-item dropdown">
@@ -39,7 +39,7 @@
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="/dbSite/Retailer/customers/index.php">Customers</a>
             <a class="dropdown-item" href="/dbSite/Retailer/Salesperson/index.php">Salesperson</a>
-            <a class="dropdown-item" href="/dbSite/Retailer/Product/index.php">Product</a>
+            <a class="dropdown-item" href="/dbSite/Retailer/product/index.php">Product</a>
             <a class="dropdown-item" href="/dbSite/Retailer/Users/index.php">Users</a>
           </div>
           </li>
